8a3d0945a881fdc9000bfdf39b3013ff78542ab1
Celot1979 <zatak631979@gmail.com>
2021-06-09 22:38:31 +0200

    Clases Adaptadoras

---
+++ src/Eventos_IV_Clases_Adaptadoras/Adptadoras.java
@@ -0,0 +1,35 @@
+package Eventos_IV_Clases_Adaptadoras;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class Adptadoras {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MarcoEvento mimarco = new MarcoEvento();
+		mimarco.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		mimarco.addWindowListener(new EventosDeVentana());
+		
+
+	}
+
+}
+
+class MarcoEvento extends JFrame{
+	public MarcoEvento() {
+		setBounds(300,300,650,350);
+		setVisible(true);
+	}
+}
+
+class EventosDeVentana extends WindowAdapter{
+	
+	public void windowClosed(WindowEvent e) {
+		System.out.println("Gracias por trabajar en la app.Hsta pronto.");
+	}
+	
+}
