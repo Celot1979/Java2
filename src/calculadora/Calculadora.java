47fc72764cddcf90145707c793b54067a40952a1
Daniel <67976795+Celot1979@users.noreply.github.com>
2021-07-21 20:18:39 +0200

    Teclado_Calculadora

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,7 +1,66 @@
 package calculadora;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+
+import javax.swing;
+
 public class Calculadora {
     public static void main(String[] args) {
-        
+         Marco mimarco = new Marco();
+         mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
+class Marco extends JFrame{
+    public Marco(){
+        setTitle("Calculadora");
+        setBounds(600,500,400,400);
+        laminaCaja lamina = new laminaCaja();
+        
+
+        
+        add(lamina,BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+}
+
+class laminaCaja extends JPanel{
+    public laminaCaja() {
+        setLayout(new BorderLayout());
+        JButton pantalla =new JButton("0");
+        add(pantalla, BorderLayout.NORTH);
+
+        JPanel numeracion = new JPanel();
+        numeracion.setLayout(new GridLayout(4,4));
+        numeracion.add(new JButton("7"));
+        numeracion.add(new JButton("8"));
+        numeracion.add(new JButton("9"));
+        numeracion.add(new JButton("X"));
+        numeracion.add(new JButton("4"));
+        numeracion.add(new JButton("5"));
+        numeracion.add(new JButton("6"));
+        numeracion.add(new JButton("-"));
+        numeracion.add(new JButton("1"));
+        numeracion.add(new JButton("2"));
+        numeracion.add(new JButton("3"));
+        numeracion.add(new JButton("+"));
+        numeracion.add(new JButton("0"));
+        numeracion.add(new JButton("',"));
+        numeracion.add(new JButton("="));
+
+        add(numeracion, BorderLayout.CENTER);
+
+        
+
+
+    }
+}
+
