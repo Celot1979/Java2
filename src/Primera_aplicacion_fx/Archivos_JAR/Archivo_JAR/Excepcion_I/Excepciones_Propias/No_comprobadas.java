81497cc742bf00a2fce98dffc2066cd58357c46f
DaniGilMar <90446291+DaniGilMar@users.noreply.github.com>
2021-09-21 20:42:09 +0200

    Paso_Uno

---
+++ src/Primera_aplicacion_fx/Archivos_JAR/Archivo_JAR/Excepcion_I/Excepciones_Propias/No_comprobadas.java
@@ -0,0 +1,10 @@
+package Primera_aplicacion_fx.Archivos_JAR.Archivo_JAR.Excepcion_I.Excepciones_Propias;
+/*
+Vamos a intentarlo hacerlo con entrada por consola, porque con JOptionPane, da error. En Eclipse Si está así, de hecho 
+se guarda en disco duro con esa fórmula
+ */
+public class No_comprobadas {
+    public static void main(String[] args){
+
+    }
+}
