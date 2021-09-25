4bffc2d394adc56c87819c0fbe4e03131e1dc7e5
DaniGilMar <90446291+DaniGilMar@users.noreply.github.com>
2021-09-25 12:27:42 +0200

    Cerrar Entrada

--- src/Streams_Datos/Acceso_Lecturas_Ficheros.java
+++ src/Streams_Datos/Acceso_Lecturas_Ficheros.java
@@ -4,27 +4,28 @@ import java.io.FileReader;
 
 public class Acceso_Lecturas_Ficheros {
 
-    lectura lex = new lectura();
-    lex.leer();
+    Lectura lex = new  Lectura();
+    lex.leerDatos();
 
 }
 
-class lectura(){
+class Lectura {
 
-    public void leer(){
-        try{
+    public void leerDatos() {
+        try {
             FileReader entrada = new FileReader("/Users/danielgil/Desktop/Practica.txt");
             int caracteres = entrada.read();
             char letra = (char) caracteres;
-            while(caracteres != -1){
+            while (caracteres != -1) {
                 System.out.println(letra);
                 caracteres = entrada.read();
                 letra = (char) caracteres;
 
             }
-        }catch(Exception e){
+            entrada.close();
+        } catch (Exception e) {
             System.out.println("No hemos encontrado el archivo");
         }
-        
+
     }
 }
