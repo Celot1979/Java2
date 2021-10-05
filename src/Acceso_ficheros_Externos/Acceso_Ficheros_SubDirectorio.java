36affe98f45df90eeb2346f1545186ff33262978
DaniGilMar <90446291+DaniGilMar@users.noreply.github.com>
2021-10-05 17:04:18 +0200

    Acceso de archivos_Subcarpertas

---
+++ src/Acceso_ficheros_Externos/Acceso_Ficheros_SubDirectorio.java
@@ -0,0 +1,22 @@
+package Acceso_ficheros_Externos;
+
+import java.io.File;
+
+public class Acceso_Ficheros_SubDirectorio {
+    public static void main(String[] args){
+        File subdirectorio = new File("/Users/danielgil/Desktop/Material acceso");
+        String[] archivos =subdirectorio.list();
+        for(int i=0; i<archivos.length; i++){
+            System.out.println(archivos[i]);
+            File archivosSubdirectorio = new File(subdirectorio.getAbsolutePath(), archivos[i]);
+            if(archivosSubdirectorio.isDirectory()){
+                String [] otros = archivosSubdirectorio.list();
+                for(int j=0; j<otros.length; j++){
+                    System.out.println(otros[j]);
+
+                }
+            }
+
+        }
+    }
+}
