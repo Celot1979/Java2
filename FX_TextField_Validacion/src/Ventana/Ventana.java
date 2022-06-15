1c5b58cd4e6cf34f5348735623bd0d8b970b13c5
Daniel <67976795+Celot1979@users.noreply.github.com>
2022-06-15 12:01:02 +0200

    Validación de TextField

---
+++ FX_TextField_Validacion/src/Ventana/Ventana.java
@@ -0,0 +1,36 @@
+package Ventana;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class Ventana {
+	public static void mostrar(String mensaje, String Titulo) {
+		Stage miStage = new Stage();
+		miStage.initModality(Modality.APPLICATION_MODAL);
+		
+		miStage.setTitle(Titulo);
+		miStage.setMinWidth(250);
+		Label miLabel = new Label();
+		miLabel.setText(mensaje);
+		
+		Button miBoton = new Button();
+		miBoton.setText("Cerrar");
+		
+		miBoton.setOnAction(e -> miStage.close());
+		
+		BorderPane miPane =  new BorderPane();
+		Scene miScene = new Scene(miPane,150,50);
+		
+		miPane.setTop(miLabel);
+		miPane.setCenter(miBoton);
+		
+		miStage.setScene(miScene);
+		
+		miStage.showAndWait();
+	}
+
+}
---
+++ FX_TextField_Validacion/src/module-info.java
@@ -0,0 +1,6 @@
+module FX_TextField_Validacion {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
