799f7e1d50749ff36f89ed728d30ba8f6ba5bb5b
Daniel <67976795+Celot1979@users.noreply.github.com>
2022-07-01 19:40:57 +0200

    Aplicación finalizada

--- FX_ListView_Caso_Practico/src/application/Main.java
+++ FX_ListView_Caso_Practico/src/application/Main.java
@@ -31,108 +31,99 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------	
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------- T E X T F I E L D -------------------------------------------------------------------------------------------------------			
+	        // TextField (Existe un método al final que crea el componente)
+			nom = Crear_TextField(nom,"Nombre");
+			tel = Crear_TextField(tel,"Telefono");
+			dire= Crear_TextField(dire,"Dirección");
 			
-			// TextField (Existe un método al final que crea el componente)
-			nom = caja_Texto(nom,"Nombre");
-			tel = caja_Texto(tel,"Telefono");
-			dire= caja_Texto(dire,"Telefono");
-			
-			// RadioButton Tamaño
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------	
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------T O G G L E G R O U P   Y   R A D IO B U T T O N -------------------------------------------------------------------------
 			
 			// Crear los toggles groups
 	        ToggleGroup grupo = new ToggleGroup();
 	        ToggleGroup grupo2 = new ToggleGroup();
 	        
 	        //Creamos los RadioButtons (Existe un método al final que crea el componente)
-	        peque = opcion(peque,"Pequeña",grupo);
-	        mediana = opcion(mediana,"Mediana",grupo);
-	        grande = opcion(grande,"Grande",grupo);
-	        fina =  opcion(fina,"Fina",grupo2);
-	        gruesa= opcion(gruesa,"Gruesa",grupo2);
+	        peque = Crear_RadioButton(peque,"Pequeña",grupo);
+	        mediana = Crear_RadioButton(mediana,"Mediana",grupo);
+	        grande = Crear_RadioButton(grande,"Grande",grupo);
+	        fina =  Crear_RadioButton(fina,"Fina",grupo2);
+	        gruesa= Crear_RadioButton(gruesa,"Gruesa",grupo2);
+	        vacio= Crear_RadioButton(vacio,"Extra-Gruesa",grupo2);
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------	
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------L I S T V I E W------------------------------------------------------------------------------------------	
+	       /*
+	        * 1º INICIALIZAMOS 1º EL COMPONENTE ListView(). 
+	        * 2º AGREGAMOS LOS ITEMS -INGREDIENTES -.
+	        * 3º IMPLEMENTAMOS MÉTODOS QUE NOS PERMITEN:
+	        *    A) SELECCIONAR VARIOS ITEMS. 
+	        *    B) QUE TENGAN UN TAMAÑO CONCRETO EN LA INTERFAZ GRÁFICA (CON SCROLL ). 
+	        *    
+	        * 4º IMPORTANTE:
+	        *    - EL SIGUIENTE PASO ES IRNOS AL MÉTODO QUE GUARDA LO SELECCIONADO POR EL USUARIO EN UNA ObservableList.
+	        *    UNA VEZ REALIZADO, SE RECORRE CON UN BUCLE FOR Y SE GUARDA EN UNA VARIABLE STRING - MÉTODO SE LLAMA ( envioList() -.
+	        *    
+	        * 5º IREMOS AL MÉTODO QUE DESENCADENA LA ACCIÓN AL PRESIONAR EL BOTÓN.
+	        *    - INICIALIZAMOS EL MÉTODO - envioList() - .
+	        *    - INCREMENTAMOS EL STRING - mensaje - CON LA SALIDA DEL STRING - menssage - ( String que nos muestra lo seleccionado).
+	        * 
+	        */
+	        Lista_Vi = new ListView<String>();
+	       
+	        //Añadimos los productos a la lista observable
+	        Lista_Vi.getItems().addAll("Peperoni","Queso","Pimiento","Aceitunas","Champiñones","Tomates","Anchoas","Jamón","Piña");
 	        
-	        // Creamo el ListView
-	        ListView <String> miLista = new ListView <String> ();
-	        String [] comida = {"Peperoni","Queso","Pimiento","Aceitunas","Champiñones","Tomates","Anchoas","Jamón","Piña"};
-			miLista.getItems().addAll(comida);
-			
+	        // Modo de escoger multiproducto
+	        Lista_Vi.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+	        
 			// Tamaño de la lista en la app
-			miLista.setPrefHeight(65);
-			miLista.setPrefWidth(20);
-			
-			// Método de llamada a la acción
-			
-			
-			miLista.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-
-				@Override
-				public void changed(ObservableValue<? extends String> ov, String old_val, String new_val) {
-					// TODO Auto-generated method stub
-					
-					/*resultado = new Label("");
-					//intento = (String) ((ObservableValue<String>)new_val).getValue();
-					resultado.setText(new_val);
-					ArrayList <String> rastreo = new ArrayList<String>();
-					intento = (String) resultado.getText();
-					rastreo.add(new_val);
-					
-				   System.out.println(rastreo.size());*/
-					miLista.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-					resultado = new Label("");
-					resultado.setText(new_val);
-					resultado.setText(new_val);
-					resultado.requestFocus();
-				}
-				
-			});
-				
-			
+	        Lista_Vi.setPrefHeight(68);
+	        Lista_Vi.setPrefWidth(20);
+	        
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------	
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------- B O T O N E S-----------------------------------------------------------------------------------------
 			// Botónes (Existe un método al final que crea el componente)
-			Accion = botones(Accion,"Enviar", primaryStage); 
-			Salir = botones(Salir,"Salir", primaryStage); 
-
-			// Creación Pane del titulo
-			HBox paneTitulo = new HBox(20, etiqueta(Titulo,"PIDE TU PIZZA","Titulo"));
-			paneTitulo.setPadding(new Insets(8));
-			// Creación Pane Equipo
-
-			HBox paneNombre = new HBox(20, etiqueta(nombre,"NOMBRE","peque"), nom);
-			paneNombre.setPadding(new Insets(8));
-
-			// Creación Pane Jugador
-			HBox paneTelf = new HBox(20, etiqueta(telefono,"TELEFONO","peque"), tel);
-			paneTelf.setPadding(new Insets(8));
-
-			// Creación Pane Puntos
-			HBox paneDire = new HBox(20,  etiqueta(direccion,"Direccion","peque"),dire);
-			paneDire.setPadding(new Insets(8));
-
+			Accion = Crear_Botones(Accion,"Enviar", primaryStage); 
+			Salir = Crear_Botones(Salir,"Salir", primaryStage); 
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------	
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------H B O X   Y   V B O X---------------------------------------------------------------------------------------
+			VBox Datos = new VBox (
+					Crear_HBOX_Horizontal(20,Crear_Label(Titulo,"PIDE TU PIZZA","Titulo"), nom),
+					Crear_HBOX_Horizontal(20, Crear_Label(nombre,"NOMBRE","peque"), nom),
+					Crear_HBOX_Horizontal(20, Crear_Label(telefono,"TELFONO","peque"), tel),
+					Crear_HBOX_Horizontal(20,  Crear_Label(direccion,"DIRECCIÓN","peque"),dire)
+					);
+			
 			// Creación Pane Botón
 			HBox paneBoton = new HBox(20, Accion,Salir);
 			paneBoton.setPadding(new Insets(8));
 			paneBoton.setAlignment(Pos.BOTTOM_RIGHT);
 			
-			// Creación de HBox y VBox para añadir los RadioButton del tamaño de la pizza.
-			VBox VerticalTama = new VBox(etiqueta(TituloTama,"TAMAÑO","titulos_peque"),peque,mediana,grande);
-			//HBox HorizontalComp = new HBox(VerticalTama);
-			VerticalTama.setPadding(new Insets(8));
-			// Creación de HBox y VBox para añadir los RadioButton de la masa  pizza.
-			VBox VerticalMasa = new VBox(etiqueta(TituloMasa,"TIPO DE MASA","titulos_peque"), fina,gruesa);
-			VerticalMasa.setPadding(new Insets(8));
-			// Creación de HBox y VBox para añadir los RadioButton de la masa pizza.
-			
-			VBox ingredientes_lista = new VBox(20,miLista);
+
+			VBox ingredientes_lista = new VBox(20,Lista_Vi);
 			ingredientes_lista.setPadding(new Insets(8));
-			VBox V_Ingredientes = new VBox(etiqueta(TituloIngre,"INGREDIENTES","titulos_peque"),ingredientes_lista);
-			V_Ingredientes.setPadding(new Insets(8));
-			
 			
 			// Creación de HBox general Horizontal con los tres bloques
-			HBox HorizontalCompMasa = new HBox(VerticalTama,VerticalMasa,V_Ingredientes);
+			HBox HorizontalCompMasa = new HBox(
+					 Crear_VBOX_Vertical(Crear_Label(TituloTama,"TAMAÑO","titulos_peque"),peque,mediana,grande),
+					 Crear_VBOX_Vertical(Crear_Label(TituloMasa,"TIPO DE MASA","titulos_peque"),fina,gruesa,vacio),
+					 Crear_VBox_Vertical_ListView(Crear_Label(TituloIngre,"INGREDIENTES","titulos_peque"),Lista_Vi )
+					);
 			
 			
 			// Creación del VBox( Ordena el conjunto entero dentro de la escena)
-			VBox panelVertical = new VBox(8,paneTitulo, paneNombre, paneTelf, paneDire,HorizontalCompMasa, paneBoton);
-
+			VBox panelVertical = new VBox(8,Datos,HorizontalCompMasa, paneBoton);
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------	
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------ CREACIÓN DE ESCENA --------------------------------------------------------------------------
+			
 			// Creación de la escena
 			Scene miScene = new Scene(panelVertical);
 			primaryStage.setScene(miScene);
@@ -143,8 +134,12 @@ public class Main extends Application {
 		}
 	}
 	
+	// ----------------------------------------------------------------------------------------------------------------------------------------------------------	
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------- MÉTODOS ---------------------------------------------------------------------------------------
+	// Método que está a la escucha y desencadena la acción después de presionar el botón -Enviar -
 	public void acciones() {
-		String mensaje = "Nombre: " + nom.getText().toUpperCase() + "\nTeléfono: " + tel.getText() + "\nDirección: " + dire.getText().toUpperCase() + 
+		mensaje = "Nombre: " + nom.getText().toUpperCase() + "\nTeléfono: " + tel.getText() + "\nDirección: " + dire.getText().toUpperCase() + 
 				"\nTamaño de la pizza es: " ;
 		String Titulo_Ventana= " Pedido Realizado";
 		if(peque.isSelected())mensaje += "PEQUEÑA";
@@ -154,15 +149,27 @@ public class Main extends Application {
         if(fina.isSelected())mensaje += "FINA";
 		if(gruesa.isSelected()) mensaje += "GRUESA";
 		mensaje += "\nLos ingredientes seleccionado son: \n";
-		intento = resultado.getText();
-		
-		mensaje+= intento + "\n";
-
+		Mostrar_Contenido_ListView();
+		mensaje += menssage.toUpperCase();
 		ventanaEmergente.mostrar(mensaje, "Datos del pedido !!!");
 		//System.out.println(mensaje);
 	} 
+	/* Método encargado  de:
+	 * A). Almacenar la información seleccionada de la - ListView - en una - ObservableList -
+	 * B). Realizamos un ciclo for para ir sacando Item por Item.
+	 * C). La almacenamos en la Variable menssage.
+	 * D). Está variable será sumada en la - variable mensaje (Método acciones)-.
+	 */
+	public void Mostrar_Contenido_ListView() {
+		menssage = "";
+		list_Obser = Lista_Vi.getSelectionModel().getSelectedItems();
+		for(String i : list_Obser) {
+			menssage += i + "\n";
+		}
+	}
 	
-	public Label etiqueta(Label Y, String Titulo, String Tipo) {
+	// Método para crear un Label.Se pasa por parámetro - Label,Titulo, fuente personalizada.
+	public Label Crear_Label(Label Y, String Titulo, String Tipo) {
 		Y =  new Label(Titulo);
 		Y.setMinWidth(200);
 		Y.setPrefWidth(100);
@@ -176,22 +183,25 @@ public class Main extends Application {
 		}
 		return Y;
 	}
-	public TextField caja_Texto(TextField X, String Titulo) {
+	// Método para crear un TextField .Se pasa por parámetro un TextField y el titulo que saldrá en la casilla - titulo orientativo-.
+	public TextField Crear_TextField(TextField X, String Titulo) {
 		X = new TextField();
 		X.setMinWidth(200);
 		X.setMaxWidth(200);
 		X.setPromptText(Titulo);
 		return X;
 	}
-	
-	public RadioButton opcion(RadioButton P,String Titulo,ToggleGroup grupo) {
+	// Método para crear un RadioButton  .Se pasa por parámetro un RadioButton y el titulo será lo visible para el usuario, Grupo al que pertenecerá .
+	public RadioButton Crear_RadioButton(RadioButton P,String Titulo,ToggleGroup grupo) {
 		P =  new RadioButton(Titulo);
 		P.setPadding(new Insets(8));
 		P.setToggleGroup(grupo);
 		return P;
 	}
-	
-	public Button botones(Button Accion,String Titulo,Stage primaryStage) {
+	/* Método para crear un  Button. Se pasa por parámetro un Button y el titulo será lo visible para el usuario.
+	 * Como nota particular, en esté caso también se le pasa un Stage para que pueda funcionar el método del botón - Salir-.
+	 */
+	public Button Crear_Botones(Button Accion,String Titulo,Stage primaryStage) {
 		Accion  = new Button(Titulo);
 		Accion .setMinWidth(80);
 		Accion .setMaxWidth(80);
@@ -199,44 +209,50 @@ public class Main extends Application {
 		if(Titulo.equals("Salir")) Accion.setOnAction(e -> primaryStage.close());
 		return Accion;
 	}
+	/* Método para crear un HBox. Se pasa por parámetro un número que separará a los componentes.
+	 * El titulo al crear internamente un Label - usado el método - Crear_Label - .
+	 * Al tener que reflejar los TextField de los datos personales, tenemos que pasarle el un TextField ( creados con el método Crear_TextField).
+	 */
+	public HBox Crear_HBOX_Horizontal(int num,Label L,TextField T) {
+		HBox horizontal = new HBox(num,L,T);
+		horizontal.setPadding(new Insets(8));
+		return horizontal;
+	}
+	/* Método para crear un VBox. Se pasa por parámetro:
+	 * El titulo al crear internamente un Label - usado el método - Crear_Label - .
+	 * También se pasa por parámetro los RadioButton ( creados con el método Crear_RadioButton)
+	 */
+	public VBox Crear_VBOX_Vertical(Label L,RadioButton U,RadioButton N,RadioButton O) {
+		VBox Vertical = new VBox(L,U,N,O);
+		Vertical.setPadding(new Insets(8));
+		return Vertical;
+	}
+	/* Método para crear unVBox. Se pasa por parámetro:
+	 * 
+	 * (Label L,  ListView<String> Lista_Vi)
+	 * Esto sucede por que nace la necesidad de pasarle una ListView.
+	 */
+	public VBox Crear_VBox_Vertical_ListView(Label L,  ListView<String> Lista_Vi) {
+		VBox Vertical = new VBox(L, Lista_Vi);
+		Vertical.setPadding(new Insets(8));
+		return Vertical;
+	}
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------	
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 	private Stage primaryStage;
 	private TextField nom,tel,dire;
-	private RadioButton peque,mediana,grande,fina,gruesa;
+	private RadioButton peque,mediana,grande,fina,gruesa,vacio;
 	private CheckBox peperoni,queso,pimiento,aceitunas,champoñones,tomates,anchoas,jamon,pina;
 	private Label Titulo,nombre,telefono,direccion,ingredientes,TituloMasa,TituloTama,TituloIngre,Titulo_Regis,resultado;
 	private Font fuente,titulos_peque;
 	private ToggleGroup grupo;
 	private Button Accion,Salir;
-	//private String [] comida = {"Peperoni","Queso","Pimiento","Aceitunas","Champiñones","Tomates","Anchoas","Jamón","Piña"};
-	//private ListView <String> miLista ;
-	private String variable_List,intento;
+	private  ListView<String> Lista_Vi ;
+	ObservableList<String> list_Obser;
+	private String variable_List,intento,mensaje, menssage;
 }
-
-
-//Creación de los CheckBox Ingredientes
-
-/*peperoni= new CheckBox("Peperoni");
-queso = new CheckBox("Queso");
-pimiento = new CheckBox("Pimiento");
-aceitunas= new CheckBox("Aceitunas");
-champoñones= new CheckBox("Champiñones");
-tomates= new CheckBox("Tomates");
-anchoas= new CheckBox("Anchoas");
-jamon= new CheckBox("Jamón");
-pina= new CheckBox("Piña");*/
-
-
-//Dentro método acciones
-/*mensaje+= "\nLos ingredientes escogidos son:  ";
-if(peperoni.isSelected())mensaje += "Peperoni ";
-if(queso.isSelected()) mensaje += "Queso ";
-if(pimiento.isSelected()) mensaje += "Pimiento ";
-if(aceitunas.isSelected())mensaje += "Aceitunas ";
-if(champoñones.isSelected()) mensaje += "Champoñones ";
-if(tomates.isSelected()) mensaje += "Tomates ";
-if(anchoas.isSelected()) mensaje += "Anchoas ";
-if(jamon.isSelected())mensaje += "Jamón ";
-if(pina.isSelected()) mensaje += "Piña ";*/
